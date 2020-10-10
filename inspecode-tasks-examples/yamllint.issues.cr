a2ca8fe657762ec226a165ea1ede9ded4f7b8a67
Katsuya ABO <z-abo@sra.co.jp>
2020-10-11 07:27:33 +0900

    test using examples with CR, CRLF, LF line terminators

---
+++ inspecode-tasks-examples/yamllint.issues.cr
@@ -0,0 +1,1 @@
+{{file:./.travis.yml}}:{{startLine:1}}:{{startColumn:1}}: [{{level:warning}}] {{message:missing document start "---" (document-start)}}{{file:./rocro.yml}}:{{startLine:15}}:{{startColumn:7}}: [{{level:error}}] {{message:wrong indentation: expected 8 but found 6 (indentation)}}{{baseDir=/home/rocro}}{{lineOriginZero=false}}{{columnOriginZero=false}}{{levelMap= {} }}
