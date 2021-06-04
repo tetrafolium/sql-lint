95cf71915d7695a31206bb691a9bb62eed2a7537
Inspecode <inspecode-bot@rocro.com>
2021-06-04 07:23:08 +0000

    Fix misspell errors

--- src/fixer.ts
+++ src/fixer.ts
@@ -14,7 +14,7 @@ class Fixer {
     query.lines.forEach((line) => {
       line.tokens.forEach((token) => {
         // If the token is just '', skip over it otherwise
-        // we insert unneccessary lines.
+        // we insert unnecessary lines.
         if (token.value) {
           if (token.type === "keyword") {
             fixed += `${token.value.toUpperCase()}\n`;
